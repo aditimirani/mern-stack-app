b501f2159b1b7417b8ea9399ede65e3a9cdd6e14
“Aditi <aditimirani@gmail.com>
2020-05-31 18:05:07 +1000

    fix: fix style changes

--- src/users/components/UserList.js
+++ src/users/components/UserList.js
@@ -9,13 +9,15 @@ const UserList = props => {
 
     if(props.item.length === 0){
         return(
+            <div className='center'>
             <Card>
                 <h2 className='center'>No User Found</h2>
             </Card>
+            </div>
         )
     }
     return(
-        <ul>
+        <ul className='users-list'>
             {props.item.map(user => {
                 const {id, name, image, places} = user;
                 return <UserItem key={id} id={id} name={name} image={image} placeCount={places}/>
