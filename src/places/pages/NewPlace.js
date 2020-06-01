28ec9d079e71b90eea8faa17976780ae3ea01f7b
“Aditi <aditimirani@gmail.com>
2020-06-01 22:56:23 +1000

    feat: added input validation

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -1,11 +1,17 @@
 import React from 'react';
+
 import Input from '../../shared/components/FormElements/Input';
+import { VALIDATOR_REQUIRE } from '../../shared/components/util/validators';
 
 import './NewPlace.css';
 
 const NewPlace = () =>{
     return(<form className='place-form'>
-        <Input element='input' type='input' label='Title' />
+        <Input element='input' 
+            type='input' 
+            label='Title' 
+            validators={[VALIDATOR_REQUIRE()]} 
+            errorText='please enter a valid title'/>
     </form>)
 }
 export default NewPlace;
