079184ceb883dc9dc5b3b1d450839dbe5ef0ae2c
“Aditi <aditimirani@gmail.com>
2020-06-04 18:52:00 +1000

    feat: added button to redirect to new place and style fix

--- src/places/components/PlaceList.js
+++ src/places/components/PlaceList.js
@@ -3,16 +3,17 @@ import {Link} from 'react-router-dom';
 
 import Card from '../../shared/components/UIElements/Card';
 import PlaceItem from './PlaceItem';
+import Button from '../../shared/components/FormElements/Button';
 
 import './PlaceList.css';
 
 const PlaceList = (props) => {
     if(props.items.length === 0){
         return (
-            <div className='place-list-center'>
+            <div className='place-list center'>
                 <Card>
                     <h2> No Places found. Maybe create one?</h2>
-                    <Link> Share places</Link>
+                    <Button to='/places/new'> Share places</Button>
                 </Card>
             </div>
         )
